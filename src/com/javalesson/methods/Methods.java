d316e54b14e0d9fa3269fcc3a7f3fe50b9d0e66d
777AMR <meyram.ramazanov@mail.ru>
2020-06-28 14:43:30 +0600

    Added sout "Hello"

--- src/com/javalesson/methods/Methods.java
+++ src/com/javalesson/methods/Methods.java
@@ -3,14 +3,23 @@ package com.javalesson.methods;
 public class Methods {
     public static void main(String[] args) {
         String str1 = "I like coffee";
+        String str3 = "I like coffee!!!";
 
         System.out.println(str1.toUpperCase());
         System.out.println(str1.toLowerCase());
 
-        String str2 = str1;
-        boolean b = str2 == str1;
-        System.out.println(b);
+        String str2 = "I like coffee";
+        String str4 = new String("I like coffee");
 
+
+        String substring = str3.substring(0, 13);
+        System.out.println("Substring = " + substring);
+
+//        boolean b = str2.equals(str4);
+//        System.out.println(b);
+
+        boolean like = str1.startsWith(" like", 1);
+        System.out.println(like);
 
     }
 }
