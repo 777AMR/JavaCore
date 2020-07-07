4a565ca3a43763d71eca4075bf5ee3fa3f15c600
777AMR <meyram.ramazanov@mail.ru>
2020-07-07 17:20:58 +0600

    collections

--- src/com/javacore/collections/ArrayLauncher.java
+++ src/com/javacore/collections/ArrayLauncher.java
@@ -1,10 +1,40 @@
 package com.javacore.collections;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayLauncher {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int[] myArray = {0,100,200,300,400,500};
-        for (int i = 0; i < myArray.length; i++) {
-            System.out.println("Element " + i + " = " + myArray[i]);
+        int[] myArray = new int[8];
+
+        System.out.println("Please enter 8 int elements");
+        for (int j = 0; j < myArray.length; j++) {
+            System.out.println("Next element");
+            myArray[j] = scanner.nextInt();
+        }
+
+        int[] array = sort(myArray);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " = " + array[i]);
         }
     }
+
+    private static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+//    10, 5, 16, 4
 }
---
+++ src/com/javacore/collections/Dies.java
@@ -0,0 +1,7 @@
+package com.javacore.collections;
+
+public class Dies {
+    public static void main(String[] args) {
+
+    }
+}
