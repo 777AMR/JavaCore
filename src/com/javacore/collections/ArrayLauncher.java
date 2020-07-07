2284183c63eb10b3846f8bc0c7170b97e1429287
777AMR <meyram.ramazanov@mail.ru>
2020-07-07 16:22:30 +0600

    Added sout "Hello"

---
+++ src/com/javacore/collections/ArrayLauncher.java
@@ -0,0 +1,10 @@
+package com.javacore.collections;
+
+public class ArrayLauncher {
+    public static void main(String[] args) {
+        int[] myArray = {0,100,200,300,400,500};
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println("Element " + i + " = " + myArray[i]);
+        }
+    }
+}
