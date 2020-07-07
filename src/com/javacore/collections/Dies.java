1455f21c9eb17dc06335958697cbf06caacf9d64
777AMR <meyram.ramazanov@mail.ru>
2020-07-07 17:32:29 +0600

    double array

--- src/com/javacore/collections/Dies.java
+++ src/com/javacore/collections/Dies.java
@@ -1,7 +1,25 @@
 package com.javacore.collections;
 
+import java.security.SecureRandom;
+
 public class Dies {
     public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        int[] frequency = new int[7];
+        for (int i = 0; i < 1000; i++) {
+            ++frequency[1 + random.nextInt(6)];
+        }
+        for (int i = 1; i < frequency.length; i++) {
+            System.out.println("Side " + i + " : " + frequency[i]);
+        }
 
     }
+
+
+    private static void multidimArrayLauncher(){
+        int[][] a = {{1,2}, {3,4,5,6}, {7,8,9}};
+
+
+    }
+
 }
